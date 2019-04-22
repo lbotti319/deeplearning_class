c30b34228c16ca33c2d8a447a797e8c65b67ffc6
lbotti319 <lukembotti@gmail.com>
2019-04-22 13:56:39 -0400

    added class for Deep NN including forward prop and scoring

--- neural_net.py
+++ neural_net.py
@@ -1,7 +1,9 @@
 import numpy as np
+import pandas as pd
 import matplotlib
 from sklearn.preprocessing import normalize
 from helper_functions import get_feature_data, get_label_data, calculate_accuracy
+from sklearn.metrics import accuracy_score
 
 
 def sigma(K, Y, b):
@@ -12,7 +14,7 @@ def sigma(K, Y, b):
 
 def sigma_prime(K, Y, b):
     """
-    Inverse of tanh(x) is 1 - tanh(x)^s
+    Inverse of tanh(x) is 1 - tanh(x)^2
     source: http://ronny.rest/blog/post_2017_08_16_tanh/
     """
     a = sigma(K, Y, b)
@@ -38,6 +40,10 @@ def J_b(K, Y, b, v):
     return sigma_prime(K, Y, b) * v
 
 def J_b_T(K, Y, b, u):
+    z = sigma_prime(K, Y, b)
+    if u.shape == z.shape:
+        nm = z.shape[0]*z.shape[1]
+        return sigma_prime(K, Y, b).T.reshape((1, nm)) * u.reshape((nm, 1))[0,0]
     return sigma_prime(K, Y, b).T * u
 
 
@@ -116,5 +122,78 @@ def entropy(c, Y, K, b, w):
 
 
     return t1 + t2
+
+def entropy_gradient(c, Y, K, b, w):
+    """
+    w: nc x m matrix
+    y: nf x n matrix
+    c: nc x n matrix
+    K: m x nf
+    b: scalar
+    """
+    z = sigma(K, Y, b)
+    S = w * z
+    nc = c.shape[0]
+    n = c.shape[1]
+    enc = np.matrix(np.ones((nc,1)))
+    encT = enc.T
+
+    S_gradient = (-1/n)*(c - np.multiply(np.exp(S),enc*np.divide(1, encT*np.exp(S))))
+
+    print(S_gradient.shape)
+    z_gradient = w.T * S_gradient
+    print(z_gradient.shape)
+    K_gradient = J_K_T(K, Y, b, z_gradient).reshape(K.shape)
+    b_gradient = J_b_T(K, Y, b, z_gradient)
+    w_gradient = S_gradient * z.T
+    return {"z": z_gradient, "K": K_gradient, "b": b_gradient, "w": w_gradient}
+
+
+class Deep_NN():
+    def __init__(self, c, Y, Ks, bs, layers=1, gamma = 0.5, w_init = None):
+        self.c = c
+        self.Y = Y
+        assert len(Ks) == len(bs)
+        self.Ks = Ks
+        self.bs = bs
+        self.layers = layers
+        self.gamma = gamma
+        if w_init is not None:
+            self.w = w_init
+        else:
+            shape = (c.shape[0], Ks[-1].shape[0])
+            self.w = np.random.random(shape)
+
+    def forward_propagate(self):
+        y_i = self.Y
+        for K_i, b_i in zip(self.Ks[:-1], self.bs[:-1]):
+            # This won't run until we know what p_i is
+            y_i_1 = y_i + np.multiply(self.gamma, sigma(K_i, y_i, b_i))
+            y_i = y_i_1
+
+        return entropy(self.c, y_i, self.Ks[-1], self.bs[-1], self.w)
+
+    def classify(self, Ytest):
+        y_i = Ytest
+        for K_i, b_i in zip(self.Ks[:-1], self.bs[:-1]):
+            # This won't run until we know what p_i is
+            y_i_1 = y_i + np.multiply(self.gamma, sigma(K_i, y_i, b_i))
+            y_i = y_i_1
+
+        final_probabilities = self.w * sigma(self.Ks[-1], y_i, self.bs[-1])
+        labels = pd.DataFrame(final_probabilities).idxmax(axis=1)
+        return labels
+
+    def score(self, Ytest, cTest):
+        labels = self.classify(Ytest)
+        true_labels = pd.DataFrame(cTest).idxmax(axis=1)
+        accuracy = accuracy_score(true_labels.values, labels.values)
+        return accuracy
+
+
+
+
+
+
 
 
